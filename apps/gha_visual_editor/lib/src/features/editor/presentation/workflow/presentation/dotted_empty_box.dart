2bbc0cfbd356c83db66ca45feb0e17f98a7ed816
Masahiro Aoki <freud427@gmail.com>
2024-08-12 17:28:41 +0900

    refactor

---
+++ apps/gha_visual_editor/lib/src/features/editor/presentation/workflow/presentation/dotted_empty_box.dart
@@ -0,0 +1,41 @@
+import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/material.dart';
+import 'package:gha_visual_editor/src/constants/colors.dart';
+import 'package:gha_visual_editor/src/features/editor/presentation/workflow/presentation/workflow.dart';
+
+class DottedEmptyBox extends StatelessWidget {
+  const DottedEmptyBox({
+    super.key,
+    required this.targetColor,
+  });
+
+  final Color targetColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      key: targetRectKeySignal.value,
+      height: 300,
+      width: 340,
+      child: DottedBorder(
+        dashPattern: const [
+          6,
+          3,
+        ],
+        borderType: BorderType.RRect,
+        radius: const Radius.circular(8),
+        color: targetColor,
+        strokeWidth: 1.5,
+        child: const Center(
+          child: Text(
+            'Drag the blue connector here to \ncreate your first action.',
+            style: TextStyle(
+                color: AppColors.dotGray,
+                fontSize: 14,
+                fontWeight: FontWeight.w600),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ apps/gha_visual_editor/lib/src/features/editor/presentation/workflow/presentation/first_action.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:gha_visual_editor/src/constants/colors.dart';
+import 'package:gha_visual_editor/src/features/editor/presentation/editor_page.dart';
+import 'package:signals/signals_flutter.dart';
+
+class FirstAction extends StatelessWidget {
+  const FirstAction({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Watch(
+      (context) => Container(
+        width: 340,
+        height: 145,
+        color: Colors.transparent,
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(14),
+                  border: Border.all(
+                    color: secondBorderColor.value,
+                    width: 4,
+                  ),
+                ),
+                child: Container(
+                  width: 340,
+                  height: 130,
+                  decoration: BoxDecoration(
+                    color: AppColors.firstStepDarkGray,
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                      color: borderColor.value,
+                      width: 1.5, // Set border width to 1 pixel
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.only(left: 16.0),
+                        child: Text(
+                          'Flutter Build APK',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 4),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 16.0),
+                        child: RichText(
+                          text: TextSpan(
+                            children: [
+                              TextSpan(
+                                text: 'on ',
+                                style: TextStyle(
+                                  color: Colors.white.withOpacity(0.7),
+                                  fontSize: 14,
+                                ),
+                              ),
+                              TextSpan(
+                                text: 'push',
+                                style: TextStyle(
+                                  color: Colors.white.withOpacity(0.9),
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 18),
+                      const Divider(
+                        color: AppColors.borderBlack,
+                        thickness: 1.5,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 16.0, top: 4),
+                        child: Align(
+                          alignment: Alignment.centerRight,
+                          child: Text(
+                            'Edit',
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.white.withOpacity(0.9),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const Align(
+              alignment: Alignment.bottomCenter,
+              child: SemicircleBorderWidget(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
