d98be313ae692ec82e835c220269ccd294b9d884
Masahiro Aoki <freud427@gmail.com>
2024-10-17 19:56:42 +0900

    pass macos build

--- apps/dashboard/lib/main.dart
+++ apps/dashboard/lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:dashboard/src/common_widgets/margins.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -15,50 +18,46 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: SignUpPage(),
+      home: const SignUpPage(),
     );
   }
 }
 
-class SignUpPage extends StatefulWidget {
-  @override
-  _SignUpPageState createState() => _SignUpPageState();
-}
-
-class _SignUpPageState extends State<SignUpPage> {
-  final _formKey = GlobalKey<FormState>();
-  String _email = '';
-  String _password = '';
-  String _confirmPassword = '';
+class SignUpPage extends HookConsumerWidget {
+  const SignUpPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final emailTextController = useTextEditingController();
+    final passwordTextController = useTextEditingController();
+    final formKey = useMemoized(() => GlobalKey<FormState>());
     return Scaffold(
       appBar: AppBar(
-        title: Text('Welcome to Open CI'),
+        title: const Text('Welcome to Open CI'),
       ),
       body: Center(
         child: Container(
           width: 300,
-          padding: EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(16.0),
           child: Form(
-            key: _formKey,
+            key: formKey,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
+              children: [
                 TextFormField(
-                  decoration: InputDecoration(labelText: 'Email'),
+                  controller: emailTextController,
+                  decoration: const InputDecoration(labelText: 'Email'),
                   validator: (value) {
                     if (value == null || value.isEmpty) {
                       return 'Enter your email';
                     }
                     return null;
                   },
-                  onSaved: (value) => _email = value!,
                 ),
-                SizedBox(height: 16),
+                verticalMargin16,
                 TextFormField(
-                  decoration: InputDecoration(labelText: 'Password'),
+                  controller: passwordTextController,
+                  decoration: const InputDecoration(labelText: 'Password'),
                   obscureText: true,
                   validator: (value) {
                     if (value == null || value.isEmpty) {
@@ -66,18 +65,33 @@ class _SignUpPageState extends State<SignUpPage> {
                     }
                     return null;
                   },
-                  onSaved: (value) => _password = value!,
                 ),
-                SizedBox(height: 24),
-                ElevatedButton(
-                  child: Text('Register'),
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      _formKey.currentState!.save();
-                      // ここで登録処理を実装します
-                      print('Email: $_email, Password: $_password');
-                    }
-                  },
+                verticalMargin24,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      child: const Text('Register'),
+                      onPressed: () {
+                        if (formKey.currentState!.validate()) {
+                          formKey.currentState!.save();
+                          print(
+                              'Email: ${emailTextController.text}, Password: ${passwordTextController.text}');
+                        }
+                      },
+                    ),
+                    horizontalMargin16,
+                    ElevatedButton(
+                      child: const Text('Login'),
+                      onPressed: () {
+                        if (formKey.currentState!.validate()) {
+                          formKey.currentState!.save();
+                          print(
+                              'Email: ${emailTextController.text}, Password: ${passwordTextController.text}');
+                        }
+                      },
+                    ),
+                  ],
                 ),
               ],
             ),
