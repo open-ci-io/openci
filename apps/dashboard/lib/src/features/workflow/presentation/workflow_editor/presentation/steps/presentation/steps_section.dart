9d55abcd6ba02b14a364cc44c068369dc6ae7897
Masahiro Aoki <freud427@gmail.com>
2025-01-02 01:08:29 +0900

    Refactor steps_section.dart and _StepItem for better code organization and readability

--- apps/dashboard/lib/src/features/workflow/presentation/workflow_editor/presentation/steps/presentation/steps_section.dart
+++ apps/dashboard/lib/src/features/workflow/presentation/workflow_editor/presentation/steps/presentation/steps_section.dart
@@ -1,6 +1,7 @@
 import 'package:dashboard/src/common_widgets/margins.dart';
 import 'package:dashboard/src/features/workflow/presentation/workflow_editor/presentation/workflow_editor_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:openci_models/openci_models.dart';
 
@@ -38,9 +39,11 @@ class StepsSection extends ConsumerWidget {
             ),
             verticalMargin16,
             ReorderableListView.builder(
-              physics: const NeverScrollableScrollPhysics(),
               shrinkWrap: true,
-              itemBuilder: (context, index) {
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: steps.length,
+              onReorder: controller.reorderStep,
+              itemBuilder: (_, index) {
                 return _StepItem(
                   key: UniqueKey(),
                   step: steps[index],
@@ -48,8 +51,6 @@ class StepsSection extends ConsumerWidget {
                   workflowModel: workflowModel,
                 );
               },
-              itemCount: steps.length,
-              onReorder: controller.reorderStep,
             ),
           ],
         ),
@@ -58,7 +59,7 @@ class StepsSection extends ConsumerWidget {
   }
 }
 
-class _StepItem extends ConsumerWidget {
+class _StepItem extends HookConsumerWidget {
   const _StepItem({
     super.key,
     required this.step,
@@ -74,6 +75,10 @@ class _StepItem extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final controller =
         ref.watch(workflowEditorControllerProvider(workflowModel).notifier);
+    final stepNameTextEditingController =
+        useTextEditingController(text: step.name);
+    final commandTextEditingController =
+        useTextEditingController(text: step.command);
 
     return Card(
       elevation: 0,
@@ -85,25 +90,38 @@ class _StepItem extends ConsumerWidget {
             child: Column(
               children: [
                 TextFormField(
-                  initialValue: step.name,
+                  controller: stepNameTextEditingController,
                   decoration: const InputDecoration(
                     labelText: 'Step Name',
                     border: OutlineInputBorder(),
                   ),
-                  onChanged: (value) {
-                    controller.updateStepName(index, value);
-                  },
                 ),
                 verticalMargin16,
                 _Command(
-                  step: step,
-                  stepIndex: index,
-                  workflowModel: workflowModel,
+                  commandTextEditingController,
+                ),
+                verticalMargin8,
+                TextButton(
+                  onPressed: () {
+                    controller
+                      ..updateStepName(
+                        index,
+                        stepNameTextEditingController.text,
+                      )
+                      ..updateCommand(
+                        index,
+                        commandTextEditingController.text,
+                      );
+                  },
+                  child: const Text('Save Step'),
                 ),
                 verticalMargin8,
                 TextButton(
                   onPressed: () => controller.removeStep(index),
-                  child: const Text('Remove Step'),
+                  child: const Text(
+                    'Remove Step',
+                    style: TextStyle(color: Colors.red),
+                  ),
                 ),
               ],
             ),
@@ -115,29 +133,19 @@ class _StepItem extends ConsumerWidget {
 }
 
 class _Command extends ConsumerWidget {
-  const _Command({
-    required this.step,
-    required this.stepIndex,
-    required this.workflowModel,
-  });
+  const _Command(
+    this.commandTextEditingController,
+  );
 
-  final WorkflowModelStep step;
-  final int stepIndex;
-  final WorkflowModel workflowModel;
-
+  final TextEditingController commandTextEditingController;
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final controller =
-        ref.watch(workflowEditorControllerProvider(workflowModel).notifier);
-    final command = step.command;
-
     return TextFormField(
-      initialValue: command,
+      controller: commandTextEditingController,
       decoration: const InputDecoration(
         labelText: 'Command',
         border: OutlineInputBorder(),
       ),
-      onChanged: (value) => controller.updateCommand(stepIndex, value),
     );
   }
 }
