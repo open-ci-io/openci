0db3d314330253a0d5bec6f237093863ae77263c
Masahiro Aoki <m.aoki@open-ci.io>
2025-01-03 22:21:41 +0900

    Feature/#169 (#170)
    * feat: add step button for each step command
    
    * Refactor gitignore and build.gradle files
    
    * Refactor gitignore, build.gradle, firebase.json, firestore.rules, and flutter files
    
    * Refactor maskedFirebaseToken in formatLogs function
    
    * Refactor widget_test.dart: Remove basic Flutter widget test
    
    * Refactor pubspec.lock: Update dependencies
    
    * Update lints package to version 5.1.1 in pubspec.lock
    
    * Refactor pubspec.yaml: Update project description and version
    
    * Refactor GitHub app to handle check_run.rerequested event
    
    * Refactor GitHub app to remove unnecessary console.log statements

--- apps/firebase_functions/functions/src/github/github_app.ts
+++ apps/firebase_functions/functions/src/github/github_app.ts
@@ -78,8 +78,37 @@ const appFunction = async (app: Probot) => {
 			// For debugging purposes
 			// "pull_request.edited",
 			"push",
+			"check_run.rerequested",
 		],
-		async (context: Context<"pull_request"> | Context<"push">) => {
+		async (
+			context: Context<"pull_request"> | Context<"push"> | Context<"check_run">,
+		) => {
+			if (context.name === "check_run") {
+				const checkRunContext = context as Context<"check_run">;
+				const _payload = checkRunContext.payload;
+				const checkRunId = _payload.check_run.id;
+
+				const checkRun = await firestore
+					.collection(buildJobsCollectionName)
+					.where("github.checkRunId", "==", checkRunId)
+					.get();
+
+				if (checkRun.docs.length === 0) {
+					console.log("Check run not found");
+					return;
+				}
+
+				const buildJob = checkRun.docs[0].data();
+				await firestore
+					.collection(buildJobsCollectionName)
+					.doc(buildJob.id)
+					.update({
+						buildStatus: OpenCIGitHubChecksStatus.QUEUED,
+					});
+
+				return;
+			}
+
 			if (context.name === "pull_request") {
 				const pullRequestContext = context as Context<"pull_request">;
 				const _payload = pullRequestContext.payload;
