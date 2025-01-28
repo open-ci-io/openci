8febf655118ff8780bed63d3dc9193818e90c27f
Masahiro Aoki <freud427@gmail.com>
2025-01-28 15:43:32 +0900

    Add create pp

--- apps/openci_cli/lib/src/commands/command_runner.dart
+++ apps/openci_cli/lib/src/commands/command_runner.dart
@@ -3,6 +3,7 @@ import 'package:args/command_runner.dart';
 import 'package:cli_completion/cli_completion.dart';
 import 'package:mason_logger/mason_logger.dart';
 import 'package:openci_cli/src/commands/commands.dart';
+import 'package:openci_cli/src/commands/create_provisioning_profile.dart';
 import 'package:openci_cli/src/commands/list_procisioning_profile.dart';
 import 'package:openci_cli/src/commands/read_certificate_command.dart';
 import 'package:openci_cli/src/version.dart';
@@ -41,6 +42,7 @@ class OpenciCliCommandRunner extends CompletionCommandRunner<int> {
       );
 
     addCommand(CreateCertificateCommand());
+    addCommand(CreateProvisioningProfileCommand());
     addCommand(ReadCertificateCommand());
     addCommand(ListCertificatesCommand());
     addCommand(ListProvisioningProfileCommand());
--- apps/openci_cli/lib/src/commands/commands.dart
+++ apps/openci_cli/lib/src/commands/commands.dart
@@ -1,4 +1,5 @@
 export 'create_certificate_command.dart';
+export 'create_provisioning_profile.dart';
 export 'list_certificates_command.dart';
 export 'list_procisioning_profile.dart';
 export 'read_certificate_command.dart';
