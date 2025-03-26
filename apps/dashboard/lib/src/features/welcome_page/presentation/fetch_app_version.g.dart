17ce45082c45e1e0dfda41d50b8ae973b3866719
maoki0 <m.aoki+0@open-ci.io>
2025-03-27 01:10:42 +0900

    feat: update version to 1.1.1, add fetchAppVersion feature, and upgrade package_info_plus dependency

---
+++ apps/dashboard/lib/src/features/welcome_page/presentation/fetch_app_version.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'fetch_app_version.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$fetchAppVersionHash() => r'e738a939aaec4feaaf91a1d5bb18fe6fb3d7b076';
+
+/// See also [fetchAppVersion].
+@ProviderFor(fetchAppVersion)
+final fetchAppVersionProvider = AutoDisposeFutureProvider<String>.internal(
+  fetchAppVersion,
+  name: r'fetchAppVersionProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$fetchAppVersionHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef FetchAppVersionRef = AutoDisposeFutureProviderRef<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
