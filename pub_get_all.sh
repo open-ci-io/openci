62796daa876b27f96c0d792c58ffd41367371e31
mafreud <freud427@gmail.com>
2025-05-28 16:23:46 +0900

    feat(scripts): add pub_get_all script to fetch dependencies for multiple Dart projects

---
+++ pub_get_all.sh
@@ -0,0 +1,14 @@
+cd apps/dashboard
+dart pub get
+cd ../honyaku_konnyaku
+dart pub get
+cd ../openci_cli
+dart pub get
+cd ../openci_runner
+dart pub get
+cd ../sample_flutter_app
+dart pub get
+cd ../dashboard
+dart pub get
+cd ../../packages/openci_models
+dart pub get
