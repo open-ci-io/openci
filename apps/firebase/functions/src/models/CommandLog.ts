5ac8f8059827b657b41d232dd489bf467501fdb7
mafreud <freud427@gmail.com>
2025-07-01 17:27:34 +0900

    remove unused code

--- apps/firebase/functions/src/models/CommandLog.ts
+++
@@ -1,9 +0,0 @@
-import type { Timestamp } from "firebase-admin/firestore";
-
-export interface CommandLog {
-	command: string;
-	logStdout: string;
-	logStderr: string;
-	createdAt: Timestamp;
-	exitCode: number;
-}
--- apps/firebase/functions/src/types/redact-secrets.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "redact-secrets" {
-	interface Redact {
-		map: <T>(obj: T) => T;
-		forEach: <T>(obj: T) => void;
-	}
-
-	function redactSecrets(replacement: string): Redact;
-	export = redactSecrets;
-}
