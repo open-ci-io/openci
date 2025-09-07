1accd693ebf526e478e1c4cbaa3b966faa76a58b
mafreud <freud427@gmail.com>
2025-09-07 23:05:15 +0900

    refactor(github_repository_handler): return GitHubRepository on creation and enhance tests
    Updated `post_github_repository` to return the created repository instead of a status code. Modified tests to validate returned repository data. Removed outdated `.idea/dataSources.xml` file.

--- packages/openci-controller/src/handlers/github_repository_handler.rs
+++ packages/openci-controller/src/handlers/github_repository_handler.rs
@@ -4,13 +4,15 @@ use sqlx::PgPool;
 use tracing::error;
 
 #[allow(dead_code)]
-pub async fn post_github_repository(external_id: i64, pool: &PgPool) -> Result<StatusCode, (StatusCode, String)> {
+pub async fn post_github_repository(external_id: i64, pool: &PgPool) -> Result<GitHubRepository, (StatusCode, String)> {
     if external_id < 0 {
         return Err((StatusCode::BAD_REQUEST, "external_id must be positive".to_string()));
     }
-    sqlx::query!(r#"
-INSERT INTO github_repositories (external_id) VALUES ($1)
-"#, external_id).execute(pool).await.map_err(|e| {
+    let result = sqlx::query_as!(
+        GitHubRepository,
+        r#"
+INSERT INTO github_repositories (external_id) VALUES ($1) RETURNING id, external_id
+"#, external_id).fetch_one(pool).await.map_err(|e| {
         error!("Failed to insert github repository: {}", e);
         (
             StatusCode::INTERNAL_SERVER_ERROR,
@@ -19,7 +21,7 @@ INSERT INTO github_repositories (external_id) VALUES ($1)
     })?;
 
 
-    Ok(StatusCode::CREATED)
+    Ok(result)
 }
 
 #[allow(dead_code)]
@@ -51,15 +53,16 @@ mod github_repository_handler_tests {
 
     #[sqlx::test]
     async fn test_post_github_repository(pool: PgPool) {
-        let external_id = 1;
-        let result = post_github_repository(external_id, &pool).await;
+        const EXTERNAL_ID: i64 = 1;
+        let result = post_github_repository(EXTERNAL_ID, &pool).await;
         assert!(result.is_ok());
-        let status_code = result.unwrap();
-        assert_eq!(status_code, StatusCode::CREATED);
+        let github_repository = result.unwrap();
+        assert_eq!(github_repository.id, 1);
+        assert_eq!(github_repository.external_id, EXTERNAL_ID);
 
-        let result = get_github_repository_by_external_id(external_id, &pool).await;
+        let result = get_github_repository_by_external_id(EXTERNAL_ID, &pool).await;
         assert!(result.is_ok());
         let repository = result.unwrap();
-        assert_eq!(repository.external_id, external_id);
+        assert_eq!(repository.external_id, EXTERNAL_ID);
     }
 }
