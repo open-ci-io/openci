32afdc7b2dae785e24f36fa009ec94fd5c2d3d50
mafreud <freud427@gmail.com>
2025-09-27 17:10:47 +0900

    Refactor configuration files for consistency and formatting

--- openci-runner/github-apps/vitest.config.ts
+++ openci-runner/github-apps/vitest.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: {
-    include: ["test/**/*.test.ts"],
-    coverage: {
-      provider: "v8",
-    },
-  },
+	test: {
+		include: ["test/**/*.test.ts"],
+		coverage: {
+			provider: "v8",
+		},
+	},
 });
