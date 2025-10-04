deaadab8e7b960f3d2a417dc19309021fe674b5f
mafreud <freud427@gmail.com>
2025-10-04 18:30:21 +0900

    feat: integrate Probot middleware for GitHub webhooks

--- openci-runner/firebase/functions/src/index.ts
+++ openci-runner/firebase/functions/src/index.ts
@@ -1,8 +1,16 @@
 import { logger } from "firebase-functions";
 import { onRequest } from "firebase-functions/https";
 import { defineSecret } from "firebase-functions/params";
+import { createNodeMiddleware, createProbot } from "probot";
+
+import app from "../../../github-apps/src/index";
 
 const firebaseServiceAccount = defineSecret("FB_SERVICE_ACCOUNT");
+
+export const probotApp = createNodeMiddleware(app, {
+	probot: createProbot(),
+	webhooksPath: "/",
+});
 
 export const githubWebhook = onRequest(
 	{ secrets: [firebaseServiceAccount] },
