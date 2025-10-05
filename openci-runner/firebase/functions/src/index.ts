699b665d61e5739a35c6b130899a05081a3a2a28
mafreud <freud427@gmail.com>
2025-10-05 17:24:33 +0900

    refactor: remove unused firebase service account and streamline GitHub webhook function

--- openci-runner/firebase/functions/src/index.ts
+++ openci-runner/firebase/functions/src/index.ts
@@ -1,43 +1,32 @@
 import { onRequest } from "firebase-functions/https";
 import { defineSecret } from "firebase-functions/params";
-
-import { logger } from "firebase-functions";
 import { createNodeMiddleware, createProbot } from "probot";
 
 import { appFn } from "../probot/index.js";
 
-const firebaseServiceAccount = defineSecret("FB_SERVICE_ACCOUNT");
 const githubAppId = defineSecret("GITHUB_APP_ID");
 const githubPrivateKey = defineSecret("GITHUB_PRIVATE_KEY");
 const githubWebhookSecret = defineSecret("GITHUB_WEBHOOK_SECRET");
 
-
-export const probotApp = createNodeMiddleware(appFn, {
-	probot: createProbot({
-		overrides: {
-			appId: githubAppId.value(),
-			privateKey: githubPrivateKey.value(),
-			secret: githubWebhookSecret.value(),
-		},
-	}),
-	webhooksPath: "/",
-});
-
 export const githubWebhook = onRequest(
 	{
-		secrets: [
-			firebaseServiceAccount,
-			githubAppId,
-			githubPrivateKey,
-			githubWebhookSecret,
-		],
+		secrets: [githubAppId, githubPrivateKey, githubWebhookSecret],
 	},
-	(req, res) => {
-		const serviceAccountJson = JSON.parse(firebaseServiceAccount.value());
-		serviceAccountJson;
-		probotApp;
+	async (req, res) => {
+		const probot = await createNodeMiddleware(appFn, {
+			probot: createProbot({
+				overrides: {
+					appId: githubAppId.value(),
+					privateKey: githubPrivateKey.value(),
+					secret: githubWebhookSecret.value(),
+				},
+			}),
+			webhooksPath: "/",
+		});
 
-		logger.info("Hello logs!", { structuredData: true });
-		res.send("Hello from Firebase!");
+		probot(req, res, () => {
+			res.writeHead(404);
+			res.end();
+		});
 	},
 );
