810fca90bb4c66564f91fe4092d20c9cb267abf3
mafreud <freud427@gmail.com>
2025-10-05 15:29:47 +0900

    refactor: comment out unused GitHub App secrets and middleware initialization

--- openci-runner/firebase/functions/src/index.ts
+++ openci-runner/firebase/functions/src/index.ts
@@ -1,15 +1,13 @@
 import { onRequest } from "firebase-functions/https";
 import { defineSecret } from "firebase-functions/params";
-import { createNodeMiddleware, createProbot } from "probot";
 
 import { logger } from "firebase-functions";
-import { appFn } from "../../../github-apps/lib/index.js";
 
 const firebaseServiceAccount = defineSecret("FB_SERVICE_ACCOUNT");
 
-const githubAppId = defineSecret("GITHUB_APP_ID");
-const githubPrivateKey = defineSecret("GITHUB_PRIVATE_KEY");
-const githubWebhookSecret = defineSecret("GITHUB_WEBHOOK_SECRET");
+// const githubAppId = defineSecret("GITHUB_APP_ID");
+// const githubPrivateKey = defineSecret("GITHUB_PRIVATE_KEY");
+// const githubWebhookSecret = defineSecret("GITHUB_WEBHOOK_SECRET");
 
 // export const probotApp = createNodeMiddleware(appFn, {
 // 	probot: createProbot({
@@ -26,25 +24,25 @@ export const githubWebhook = onRequest(
 	{
 		secrets: [
 			firebaseServiceAccount,
-			githubAppId,
-			githubPrivateKey,
-			githubWebhookSecret,
+			// githubAppId,
+			// githubPrivateKey,
+			// githubWebhookSecret,
 		],
 	},
 	(req, res) => {
 		const serviceAccountJson = JSON.parse(firebaseServiceAccount.value());
 		serviceAccountJson;
 
-	createNodeMiddleware(appFn, {
-	probot: createProbot({
-		overrides: {
-			appId: githubAppId.value(),
-			privateKey: githubPrivateKey.value(),
-			secret: githubWebhookSecret.value(),
-		},
-	}),
-	webhooksPath: "/",
-});
+// 	createNodeMiddleware(appFn, {
+// 	probot: createProbot({
+// 		overrides: {
+// 			appId: githubAppId.value(),
+// 			privateKey: githubPrivateKey.value(),
+// 			secret: githubWebhookSecret.value(),
+// 		},
+// 	}),
+// 	webhooksPath: "/",
+// });
 		logger.info("Hello logs!", { structuredData: true });
 		res.send("Hello from Firebase!");
 	},
