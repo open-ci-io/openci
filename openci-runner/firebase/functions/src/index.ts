a9086bfc1a9d38365e44cc7b5294dfa8fd17e4e6
mafreud <freud427@gmail.com>
2025-10-05 15:49:45 +0900

    refactor: restore GitHub App secrets and middleware initialization in Firebase function

--- openci-runner/firebase/functions/src/index.ts
+++ openci-runner/firebase/functions/src/index.ts
@@ -2,11 +2,15 @@ import { onRequest } from "firebase-functions/https";
 import { defineSecret } from "firebase-functions/params";
 
 import { logger } from "firebase-functions";
+import { createNodeMiddleware, createProbot } from "probot";
+
+import { appFn } from "../../../github-apps/lib/index.js";
 
 const firebaseServiceAccount = defineSecret("FB_SERVICE_ACCOUNT");
 const githubAppId = defineSecret("GITHUB_APP_ID");
-// const githubPrivateKey = defineSecret("GITHUB_PRIVATE_KEY");
-// const githubWebhookSecret = defineSecret("GITHUB_WEBHOOK_SECRET");
+const githubPrivateKey = defineSecret("GITHUB_PRIVATE_KEY");
+const githubWebhookSecret = defineSecret("GITHUB_WEBHOOK_SECRET");
+
 
 // export const probotApp = createNodeMiddleware(appFn, {
 // 	probot: createProbot({
@@ -24,8 +28,8 @@ export const githubWebhook = onRequest(
 		secrets: [
 			firebaseServiceAccount,
 			githubAppId,
-			// githubPrivateKey,
-			// githubWebhookSecret,
+			githubPrivateKey,
+			githubWebhookSecret,
 		],
 	},
 	(req, res) => {
@@ -34,16 +38,16 @@ export const githubWebhook = onRequest(
 		const ghaAppId = JSON.parse(githubAppId.value());
 		ghaAppId;
 
-// 	createNodeMiddleware(appFn, {
-// 	probot: createProbot({
-// 		overrides: {
-// 			appId: githubAppId.value(),
-// 			privateKey: githubPrivateKey.value(),
-// 			secret: githubWebhookSecret.value(),
-// 		},
-// 	}),
-// 	webhooksPath: "/",
-// });
+	createNodeMiddleware(appFn, {
+	probot: createProbot({
+		overrides: {
+			appId: githubAppId.value(),
+			privateKey: githubPrivateKey.value(),
+			secret: githubWebhookSecret.value(),
+		},
+	}),
+	webhooksPath: "/",
+});
 		logger.info("Hello logs!", { structuredData: true });
 		res.send("Hello from Firebase!");
 	},
