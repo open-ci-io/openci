962bcba1bbd891564c7b1ebc670b8029286cea28
Masahiro Aoki <m.aoki@openci.org>
2026-09-08 15:27:05 +0900

    feat: update Docker Compose commands to stop dispatcher and start worker services (#2647)

--- apps/genuineci_cli/lib/src/commands/dev/start_docker_compose.dart
+++ apps/genuineci_cli/lib/src/commands/dev/start_docker_compose.dart
@@ -13,7 +13,19 @@ typedef DockerComposeProcessRunner =
       required Map<String, String> environment,
     });
 
-const _dockerComposeArguments = ['compose', 'up', '-d', '--build'];
+const _dockerComposeCommands = [
+  ['compose', 'stop', 'build-job-dispatcher'],
+  [
+    'compose',
+    'up',
+    '-d',
+    '--build',
+    'server',
+    'build-job-planner',
+    'build-job-worker',
+    'loki',
+  ],
+];
 
 Future<bool> startDockerCompose(
   Logger logger,
@@ -34,15 +46,17 @@ Future<bool> startDockerCompose(
   };
 
   try {
-    final exitCode = await processRunner(
-      'docker',
-      _dockerComposeArguments,
-      workingDirectory: projectRoot.path,
-      environment: composeEnvironment,
-    );
-    if (exitCode != 0) {
-      logger.stderr(t.dev.start.stepDockerComposeFailed);
-      return false;
+    for (final arguments in _dockerComposeCommands) {
+      final exitCode = await processRunner(
+        'docker',
+        arguments,
+        workingDirectory: projectRoot.path,
+        environment: composeEnvironment,
+      );
+      if (exitCode != 0) {
+        logger.stderr(t.dev.start.stepDockerComposeFailed);
+        return false;
+      }
     }
   } on ProcessException catch (error) {
     logger.stderr('${t.dev.start.stepDockerComposeFailed}\n${error.message}');
