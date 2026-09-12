bfe9f6c150cd96e6d375ed672d8c26cbcad98d43
Masahiro Aoki <m.aoki@openci.org>
2026-09-12 21:04:03 +0900

    feat(server): add Hello world endpoint (#2691)

--- apps/genuineci_server/bin/main.dart
+++ apps/genuineci_server/bin/main.dart
@@ -1,1 +1,6 @@
-void main() {}
+import 'package:genuineci_server/src/generated/serverpod.dart';
+
+Future<void> main(List<String> args) async {
+  final pod = Serverpod(args);
+  await pod.start();
+}
